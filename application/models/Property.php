bc544e54f5bb8a3d3567b0a9c4318b1391732c18
nimaJbin <nimagimail.com>
2020-10-16 02:05:33 +0330

    Add save and get Method

--- application/models/Property.php
+++ application/models/Property.php
@@ -6,7 +6,23 @@ class Property extends CI_Model
 
 {
 
-	
+	public function save( $table, $data )
+	{
+		$result = $this->db->insert($table, $data);
+		return $result ? $this->db->insert_id() : false;
+	}
+
+
+
+	public function get( $id )
+	{
+		$this->db->select( "property.*, address.*, property.id as id, address.id as propId" );
+		$this->db->where( "property.id", $id );
+		$this->db->from('property');
+		$this->db->join('address', 'address.propertyId = property.id' );
+
+		return $this->db->get()->row();
+	}
 
 
 }
